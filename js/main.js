e765fa7d25be4ff9e3287eeb645126606eb0f335
chiziivictor <chizivwokoma@gmail.com>
2022-02-17 23:47:10 +0100

    Add cv file

--- js/main.js
+++ js/main.js
@@ -1,13 +1,50 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+function scrollHeader() {
+  const header = document.getElementById('header')
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) {
+    header.classList.add('scroll-header');
+  } else {
+    header.classList.remove('scroll-header')
+  }
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal');
+const modalBtns = document.querySelectorAll('.services__button');
+const modalClose = document.querySelectorAll('.services__modal-close');
 
+// modalBtns.addEventListener('click', () => {
+//   modalViews.classList.add('active-modal');
+// });
+//
+// modalClose.addEventListener('click', () => {
+//   modalViews.classList.remove('active-modal');
+// });
+
+let modal = function(modalClick){
+  modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((mb, i) =>{
+  mb.addEventListener('click', () =>{
+    modal(i)
+  })
+})
+
+modalClose.forEach((mc) =>{
+  mc.addEventListener('click', () =>{
+    modalViews.forEach((mv) =>{
+      mv.classList.remove('active-modal')
+    })
+  })
+})
 
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
-/* Link active work */ 
+/* Link active work */
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
@@ -16,8 +53,7 @@
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== LIGHT DARK THEME ===============*/ 
+/*=============== LIGHT DARK THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
-
